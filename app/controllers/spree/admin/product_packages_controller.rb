2bdec300a2b4c81db9d62532dbc617e1dd7ce877
paras k <paras@ziggletech.com>
2019-02-18 16:38:30 +0530

    added create and update method

--- app/controllers/spree/admin/product_packages_controller.rb
+++ app/controllers/spree/admin/product_packages_controller.rb
@@ -1,8 +1,35 @@
 module Spree
   module Admin
     class ProductPackagesController < ResourceController
-      belongs_to 'spree/product', find_by: :slug
+      before_action :load_data
       before_filter :load_data
+
+      def create
+        @object.attributes = permitted_resource_params
+        @object.product_id=@product.id
+        byebug
+        if @object.save
+          invoke_callbacks(:create, :after)
+          flash[:success] = flash_message_for(@object, :successfully_created)
+         redirect_to "/admin/products/#{@product.slug}/product_packages"
+        else
+          render "new"
+          
+        end
+      end
+
+      def update
+        invoke_callbacks(:update, :before)
+        if @object.update_attributes(permitted_resource_params)
+          redirect_to "/admin/products/#{@object.product.slug}/product_packages"
+        else
+          redirect_to 'edit'
+        end
+        
+      end
+
+    
+      
 
       private
         def load_data
